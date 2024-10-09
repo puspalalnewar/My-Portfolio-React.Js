0621a30a56dad5d09e516749ebcfb4b13e7c5f49
Puspalal <puspalalnewar79@gmail.com>
2024-10-09 11:51:11 +0530

    Creating Projects Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import About from './components/About';
 import Header from './components/Header';
+import Projects from './components/Projects';
 import Tools from './components/Tools';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Header/>
       <About/>
       <Tools/>
+      <Projects/>
     </div>
   );
 }
---
+++ src/components/Projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div className='max-w-screen-lg m-auto mt-20'>
+      <p className='text-[2rem] font-medium'>My Portfolio Highlights</p>
+    </div>
+  )
+}
+
+export default Projects
