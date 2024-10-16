1ef7e37972da352ca61bae99e97ed21dd77ca5c1
Puspalal <puspalalnewar79@gmail.com>
2024-10-16 21:11:32 +0530

    Installled react-scroll and setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,28 @@ import Contact from './components/Contact';
 import Header from './components/Header';
 import Projects from './components/Projects';
 import Tools from './components/Tools';
+import { Element } from 'react-scroll';
 
 function App() {
   return (
     <div className='font-inter'>
-      <Header/>
-      <About/>
-      <Tools/>
-      <Projects/>
-      <Contact/>
+      <Header />
+      <Element name='about'>
+        <About />
+      </Element>
+      <Element name='tools'>
+        <Tools />
+      </Element>
+      <Element name='projects'>
+        <Projects />
+      </Element>
+      <Element name='contact'>
+        <Contact />
+      </Element>
+
+
+
+
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { FaBars } from "react-icons/fa";
 import { FaUserAlt } from "react-icons/fa";
+import { Link } from 'react-scroll';
 
-const header = () => {
+const Header = () => {
   return (
     <>
       <header className='h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0'>
@@ -10,15 +11,14 @@ const header = () => {
           {/* <h1 className='text-lg'>Newar</h1> */}
           <FaUserAlt className='text-[1.5rem] inline' />
           <div className="hidden md:block bg-zinc-50/10 p-3 rounded text-sm">
-            <a href="/" className='mr-5'>Home</a>
-            <a href="/" className='mr-5'>Work</a>
-            <a href="/" className='mr-5'>About</a>
-            <a href="/" className=''>Review</a>
-
+            <Link to="about" className='mr-5 cursor-pointer' smooth={true} duration={500}>About</Link>
+            <Link to="tools" className='mr-5 cursor-pointer' smooth={true} duration={500}>Tools</Link>
+            <Link to="projects" className='mr-5 cursor-pointer' smooth={true} duration={500}>Projects</Link>
+            <Link to="contact" className='cursor-pointer' smooth={true} duration={500}>Contact</Link>
           </div>
           <FaBars className='md:hidden' />
           <div className="hidden md:h-10 w-24 cursor-pointer bg-slate-50 text-black md:flex justify-center items-center rounded text-sm">
-            <a href="/contact" className=''>
+            <a href="/" >
               Contact Me
             </a>
           </div>
@@ -29,4 +29,4 @@ const header = () => {
   )
 }
 
-export default header
+export default Header
